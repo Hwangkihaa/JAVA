4125b20fc6ca93a4d92e54c9e35835236246d766
Hwangkihaa <113653717+Hwangkihaa@users.noreply.github.com>
2024-04-30 22:51:01 +0900

    Delete variable directory

--- variable/ex/VarEx10Question.java
+++
@@ -1,9 +0,0 @@
-package variable.ex;
-
-public class VarEx10Question {
-    public static void main(String[] args) {
-        System.out.println(4 + 3);
-        System.out.println(4 - 3);
-        System.out.println(4 * 3);
-    }
-}
