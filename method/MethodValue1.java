31fb099826f4c73e0e9f1a1f54794c039596c583
Hwangkihaa <113653717+Hwangkihaa@users.noreply.github.com>
2024-04-30 23:09:34 +0900

    Add files via upload

---
+++ method/MethodValue1.java
@@ -0,0 +1,13 @@
+package method;
+
+public class MethodValue1 {
+    public static void main(String[] args) {
+        int num1 = 5;
+        System.out.println(num1);
+
+    }
+
+    public static void changeNumber(int num2){
+        System.out.println(num2);
+    }
+}
